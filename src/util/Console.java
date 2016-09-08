ef71d7c93e22a85fa41a2d877bf660ba99c02361
Everaldo Gomes <everaldo.gomes@gmail.com>
2016-09-08 16:29:51 -0300

    Leitura de Datas no Console

--- src/util/Console.java
+++ src/util/Console.java
@@ -1,6 +1,9 @@
 package util;
 
 import java.io.PrintStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Console {
@@ -9,12 +12,15 @@ public class Console {
 	private static Scanner leitorNumeros;
 	private static Scanner leitorStrings;
 	private static PrintStream terminal;
-	
+	private static SimpleDateFormat dateFormatter;
+	private static String dateFormat = "dd/MM/yyyy";
 	
 	static {
 		leitorNumeros = new Scanner(System.in);
 		leitorStrings = new Scanner(System.in);
 		terminal = System.out;
+		dateFormatter = new SimpleDateFormat();
+		dateFormatter.applyPattern(dateFormat);
 	}
 	
 	public static void mensagem(String mensagem){
@@ -47,6 +53,16 @@ public class Console {
 			}
 		}
 		return numero;
+	}
+
+	public static Date lerDataObrigatoria(String mensagem) {
+		String dataString = lerStringObrigatoria(mensagem);
+		try{
+			return dateFormatter.parse(dataString);
+		} catch(ParseException e){
+			mensagem("Data inválida. Entre novamente");
+			return lerDataObrigatoria(mensagem);
+		}
 	}
 	
 }
